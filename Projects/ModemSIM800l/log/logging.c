092eff9d141cd5f188181f624d80ebc39813b107
Lukasz Gesieniec <lukasz@micromex.com.pl>
2016-01-26 12:24:15 +0100

    Developing logging subsystem... - not yet completed

--- Projects/ModemSIM800l/log/logging.c
+++ Projects/ModemSIM800l/log/logging.c
@@ -1,2 +1,19 @@
 #include "logging.h"
+#include <stdlib.h>
 
+struct sLogStruct* LogInit(){
+	int i;
+	struct sLogStruct *logData = malloc(sizeof(*logData));
+	logData->logOn = FALSE;
+	for (i=0; i < NUM_LOG_SUBSYSTEMS; i++) {
+		logData->outputLevel[i] = eNoLogging;
+	}
+	return logData;
+}
+
+void LogGlobalOn(struct sLogStruct* logData) {
+	logData->logOn = TRUE;
+}
+void LogGlobalOff(struct sLogStruct* logData) {
+	logData->logOn = FALSE;
+}
--- Projects/ModemSIM800l/log/logging.h
+++ Projects/ModemSIM800l/log/logging.h
@@ -2,10 +2,41 @@
 #ifndef __LOGGING_H
 #define __LOGGING_H
 
+#include <stdint.h>
+
 #ifdef __cplusplus
  extern "C" {
 #endif
 	 
+struct sFirmwareVersion{
+	uint8_t majorVerion;
+	uint8_t minorVersion;
+	uint16_t buildIndicator;
+};
+typedef enum {
+	FALSE,
+	TRUE
+} tBoolean;
+
+enum eLogLevel {eNoLogging, eLowLogging, eMediumLogging, eHighLogging};
+
+#define NUM_LOG_SUBSYSTEMS 2
+enum eLogSubSystem {eSubSystemSIM800L, eSubSystemGPIO};
+
+struct sLogStruct{
+tBoolean logOn;
+enum eLogLevel outputLevel[NUM_LOG_SUBSYSTEMS];
+};
+
+static struct sLogStruct gLogData;
+	 
+struct sLogStruct* LogInit(void);
+void Log(enum eLogSubSystem sys, enum eLogLevel level, char *msg);
+void LogWithNum(enum eLogSubSystem sys, enum eLogLevel level, char *msg, int number);
+void LogSetOutputLevel(enum eLogSubSystem sys, enum eLogLevel level);
+void LogGlobalOn(struct sLogStruct* logData);
+void LogGlobalOff(struct sLogStruct* logData);
+void LogVersion(struct sFirmwareVersion *v);
 
 	 
 #ifdef __cplusplus
